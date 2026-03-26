a4929b95eeb654afca2a74c0b41e7732d3f62f92
levery-srl <a.pracucci@levery.it>
2026-03-26 14:48:21 +0100

    Update page.jsx

--- app/outstatic/[[...ost]]/page.jsx
+++ app/outstatic/[[...ost]]/page.jsx
@@ -1,7 +1,3 @@
-import 'outstatic/outstatic.css'
-import { Outstatic } from 'outstatic'
-import { OstClient } from 'outstatic/client'
-export default async function Page({ params }) {
-  const ostData = await Outstatic()
-  return <OstClient ostData={ostData} params={params} />
+export default function Page() {
+  return null
 }
